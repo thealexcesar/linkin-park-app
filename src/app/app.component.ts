08c4bd8da7157c8c950883c3ec4a27780ffa4715
Alex Cesar <thealexcesar@gmail.com>
2024-08-16 20:55:14 -0300

    favicon + reestructure to assets files + add: (tailwind-css + font-awesome)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {fas} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'linkin-park-app';
+
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
 }
