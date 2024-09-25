1dc7f61e03659a4f7657846e0bf5eecfdc26a49a
alex cesar <thealexcesar@gmail.com>
2024-09-25 20:28:57 -0300

    🛠 build: environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  spotifyClientId: '8d311e1facc84efea30c09f1e43d064f',
+  spotifyClientSecret: '35ffc86b12b24e339677ba7200a50e76',
+  spotifyRedirectUri: 'http://localhost:4200/callback'
+};
